595a7e7f9eaf1a3623a1579bc0c937d3d2c4d9b7
R-Grigala <r.grigalashvili777@gmail.com>
2026-01-02 18:59:42 +0400

    nextJS Second Step

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,54 @@
 "use client";
+
 import Footer from "@/components/footer/Footer";
 import styles from "./page.module.css";
 import Image from "next/image";
-// import Text from "@/components/test/Text";
 import { useEffect, useState } from "react";
 import Link from "next/link";
 
+type Product = {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+};
+
 export default function Home() {
-  const [products, setProducts] = useState(null);
+  const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then((resp) => resp.json())
-      .then((res) => setProducts(res));
+      .then((res) => setProducts(res))
+      .catch(console.error);
   }, []);
 
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        {products?.map((item) => (
+        {products.map((item) => (
           <div className={styles.itemWrapper} key={item.id}>
             <Image
               src={item.image}
               width={100}
               height={80}
-              objectFit="contain"
-              alt="random"
+              alt={item.title}
+              style={{ objectFit: "contain" }}
             />
+
             <div>
               <h3 className={styles.title}>{item.title}</h3>
               <p className={styles.desc}>{item.description}</p>
             </div>
+
             <Link href={`/details/${item.id}`}>
-              <p>see details</p>
+              see details
             </Link>
           </div>
         ))}
       </main>
-      {/* <Text /> */}
+
       <Footer />
     </div>
   );
 }
-
-// MOUNT
-// UPDATING
-// UNMOUNT
