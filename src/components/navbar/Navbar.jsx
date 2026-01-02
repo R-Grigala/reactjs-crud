8dec3dc2b5fd1fbd7366dd1462f3140db16978a2
R-Grigala <r.grigalashvili777@gmail.com>
2026-01-02 18:55:19 +0400

    nextJS Second Step

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+"use client";
+import Link from "next/link";
+import styles from "./Navbar.module.css";
+import { usePathname } from "next/navigation";
+
+const NAV_LIST = [
+  {
+    id: 1,
+    title: "Main",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "Cart",
+    url: "/cart",
+  },
+  {
+    id: 3,
+    title: "Profile",
+    url: "/profile",
+  },
+  {
+    id: 4,
+    title: "Contact",
+    url: "/contact",
+  },
+];
+
+const Navbar = () => {
+  const pathname = usePathname();
+  return (
+    <>
+      <div className={styles.navbar}>
+        {NAV_LIST.map((item) => (
+          <div key={item.id}>
+            <Link href={item.url}>
+              <h3 className={pathname === item.url ? styles.activeTab : null}>
+                {item.title}
+              </h3>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
